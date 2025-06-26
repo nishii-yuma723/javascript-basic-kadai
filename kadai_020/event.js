c70f5593afd79d1053be8239c687a3679ef48255
nishii-yuma723 <yuma04028@icloud.com>
2025-06-26 23:29:33 +0900

    020ファイルをアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const pushBtn=document.getElementById("btn");
+const mainText=document.getElementById("text");
+
+pushBtn.addEventListener("click",()=>{
+    mainText.textContent="ボタンをクリックしました"
+});
